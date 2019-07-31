dac8701d24b61640bc162d9ba32e0b7c0c96ed20
Carlos Millett <caedus75@protonmail.com>
2019-07-31 15:37:58 -0300

    add code for N64 roms

---
+++ src/romRenamer/header/n64.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2018 Carlos Millett
+#
+# This file is part of romRenamer.
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+
+
+from .base import memLocation, Platform
+
+
+class N64(Platform):
+    def __init__(self):
+        header = memLocation(0x00, None, 4096)
+        title = memLocation(0x20, 0x34, None)
+        code = memLocation(None, None, None)
+        super().__init__(header, title, code)
